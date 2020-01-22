f8747f894566de27471241306c94835641587bf9
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-01-22 21:48:16 +0000

    BMI Calculation.py

--- BMI Calculation.py
+++ BMI Calculation.py
@@ -1,9 +1,10 @@
 #This program calculates BMI
 #based on user weight and height input
 
-weight = float(input("enter weight in kg: "))
-height = float(input("enter height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in cm: "))
 
-BMI = (weight/((height/100)**2))
+height_cm_to_m = height/100
 
-print("Your BMI is", BMI)
+BMI = weight / (height_cm_to_m **2)
+print('BMI is: {:.2f}'.format(BMI))
