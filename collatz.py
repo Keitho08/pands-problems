c75c91ec4774119db354bed81104abab4a40a7b0
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-02-06 08:05:28 +0000

    Collatz initial code added

---
+++ collatz.py
@@ -0,0 +1,11 @@
+# Keith Brazill
+# This programs asks user to input a positive
+#integer and outputs the successive values of the calculation
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+         num / 2
+else:
+    (num * 3) + 1
+
+
