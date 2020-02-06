ad45c5881df6e15ee26325a567297f8ec2071fbb
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-02-06 07:39:30 +0000

    Added code to prime

--- prime.py
+++ prime.py
@@ -2,11 +2,18 @@
 # Check if a number is prime
 # The primes are 2, 3, 5, 6, 11, 13, ...
 
-p = 347 
+p = 221
 m = 2
+isprime = True
 
 while m < p:
     if p % m == 0:
-        print(m,"divides", p, "and therefore", p, "is not prime")
+      #  print(m,"divides", p, "and therefore", p, "is not prime")
+      isprime = False
+      break 
     m = m + 1
-    
+
+if isprime:
+    print(p, "is a prime number.")
+else:
+    print(p, "is not prime.")
