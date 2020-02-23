432b0cbb3ae64a8784b729766c8ac45a638710ea
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-02-23 20:31:20 +0000

    squareroot inital code and reference

---
+++ squareroot.py
@@ -0,0 +1,5 @@
+# ref https://www.programiz.com/python-programming/examples/square-root
+
+#Write a program that takes a positive floating-point number as input 
+#and outputs an approximation of its square root. You should create 
+# a function called sqrt that does this.
