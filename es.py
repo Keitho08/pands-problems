87cb261f6d2f63a2bb43c1abdc3f1936ca5165ca
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-03-16 13:47:24 +0000

    es.py code

---
+++ es.py
@@ -0,0 +1,21 @@
+# Keith Brazill (G00387845)
+# 16th March 2020
+
+# Write a program that reads in a text file and outputs the number of e's it contains. 
+# The program should take the filename from an argument on the command line.
+# Ex: $ python es.py moby-dick.txt
+#     116960    
+
+txtfile = str(input("Insert Txt File Name: "))   
+
+with open (txtfile, "r") as myfile:
+    data = myfile.read()
+    freq = data.count("e")
+    print(freq)
+
+# References
+
+# https://docs.python.org/3/tutorial/inputoutput.html
+# https://pythonexamples.org/python-count-number-of-words-in-text-file/
+# https://docs.python.org/3/library/stdtypes.html#str.count
+# https://stackoverflow.com/questions/22694244/counting-specific-letters-or-symbols-in-a-text-file-in-python
