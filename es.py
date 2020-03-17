bed95e47aaa40b8e2319a4a9e72770641f7db941
Keitho08 <59957178+Keitho08@users.noreply.github.com>
2020-03-17 09:42:36 +0000

    Explanation's added, reference for Moby Dick Text file added

--- es.py
+++ es.py
@@ -6,12 +6,12 @@
 # Ex: $ python es.py moby-dick.txt
 #     116960    
 
-txtfile = str(input("Insert Txt File Name: "))   
+txtfile = str(input("Insert Txt File Name: "))   # the user is prompted to enter name of txt file as string
 
-with open (txtfile, "r") as myfile:
-    data = myfile.read()
-    freq = data.count("e")
-    print(freq)
+with open (txtfile, "r") as myfile:              # opens text file as myfile in read mode
+    data = myfile.read()                         # data is equal to the text read in inserted file  
+    freq = data.count("e")                       # freq is equal to count of "e" contained in inserted file
+    print(freq)                                  # prints freq i.e. number of e's answer = 116960 for Moby Dick
 
 # References
 
@@ -22,3 +22,4 @@ with open (txtfile, "r") as myfile:
 # https://stackoverflow.com/questions/22694244/counting-specific-letters-or-symbols-in-a-text-file-in-python
 # https://learn.datacamp.com/projects/38
 # https://realpython.com/read-write-files-python/
+# Version of Moby Dick text file used: https://www.gutenberg.org/files/2701/old/moby10b.txt 
